d7207ba17e2291bafa6d8fb51c84fa1cc5118d35
Anastasia <akorolevskih1@gmail.com>
2022-08-24 12:49:35 +0500

    0 tesk

---
+++ C# 1 lesson/task0/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+ Напишите программу, которая на вход принимает число и выдаёт 
+ его квадрат (число умноженное на само себя).
+*/ 
+
+int usernumber = new int();
+Console.WriteLine("Введите число: ");
+usernumber = Convert.ToInt32(Console.ReadLine()); 
+
+int result = usernumber * usernumber;
+Console.WriteLine($"Степень числа {usernumber} = {result}");
