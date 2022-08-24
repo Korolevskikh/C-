225077b0f1592c3a27d180df70dc06bc9722be13
Anastasia <akorolevskih1@gmail.com>
2022-08-24 14:41:05 +0500

    4 task

---
+++ C# 1 lesson/task4/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 
+456 -> 6 782 -> 2 918 -> 8
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = userNumber%10;
+Console.WriteLine(result);
