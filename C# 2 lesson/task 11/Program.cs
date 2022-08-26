60d98d8822ea7676cd0b22682218da6b91ec7e70
Anastasia <akorolevskih1@gmail.com>
2022-08-26 15:27:53 +0500

    task11

---
+++ C# 2 lesson/task 11/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и 
+удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+*/
+int GetRandomNumber(int minNumber, int maxNumber)
+{
+    int number = new Random().Next(minNumber, maxNumber);
+    return number;
+}
+int randomNumber = GetRandomNumber(100,1000);
+int result = 0;
+int GetDigitFromNumber (int number)
+{
+    int LeftNumber = number / 100*10;
+    int RightNumber = number % 10;
+   
+    result = LeftNumber + RightNumber;
+    return result;
+}
+int DF = GetDigitFromNumber(randomNumber);
+Console.WriteLine(randomNumber);
+ Console.WriteLine(result);
