56b2b4e7dd9787eeefc6342fc7aee9049e6eb034
Anastasia <akorolevskih1@gmail.com>
2022-08-26 20:26:26 +0500

    Задача 10

---
+++ ДЗ 2/Задача 10/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetRandomNumber(int minNumber, int maxNumber)
+{
+    int result = new Random().Next(minNumber, maxNumber);
+    return result;
+}
+int NumberBitwin(int number)
+{
+    int result = number / 10 % 10;
+    return result;
+}
+int randomNumber = GetRandomNumber(100,1000);
+int BitwinOneAndThree = NumberBitwin(randomNumber);
+Console.WriteLine(randomNumber);
+Console.WriteLine(BitwinOneAndThree);
