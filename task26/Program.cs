dc79ac12253d0ae5d9172377ce0a7eefe1259612
Anastasia <akorolevskih1@gmail.com>
2022-09-03 19:06:24 +0500

    task26 4 pz

---
+++ task26/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 26: Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.WriteLine("Введите число:  ");
+int num = int.Parse(Console.ReadLine());
+
+int HowNumber(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        // Console.Write($"{number} ");
+        count ++;
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество цифр = {HowNumber(num)}");
