76cd73e01bf1af9e0cd52459d7e76eb329666f75
Anastasia <akorolevskih1@gmail.com>
2022-09-03 20:30:22 +0500

    task28 4 pz

---
+++ task28/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+// Вычисление факториала
+Console.WriteLine("Введите N:  ");
+int N =int.Parse(Console.ReadLine());
+
+double MultiNum(int N)
+{
+    int Mult =1;
+    if (N > 0)
+    {
+        for (int i = 1; i <= N; i++)
+    {
+        Mult *=i;
+    }
+        return Mult;
+    }
+    else
+    {
+        Console.WriteLine("Неверно введено число (меньше заданного диапазона)");
+        return 0;
+    }
+}
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {MultiNum(N)}");
+
