37b6e8d23b0319bb2772511ac1125885612dde16
Anastasia <akorolevskih1@gmail.com>
2022-10-17 11:56:58 +0500

    pz 8 task53

---
+++ task53/Program.cs
@@ -0,0 +1,51 @@
+﻿/* Задача 53: Задайте двумерный массив. Напишите программу, 
+которая поменяет местами первую и последнюю строку 
+массива. */
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns);
+PrintArray(array);
+Console.WriteLine();
+int[,] arr = ChangeArray(array);
+PrintArray(arr);
+
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray( int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int change = array[0,j];
+        array[0,j] = array[array.GetLength(0)-1, j];
+        array[array.GetLength(0)-1, j] = change;
+    }
+    return array;
+}
