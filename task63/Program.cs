af8107af1eda582790ced69bb80ffb5a6634f456
Anastasia <akorolevskih1@gmail.com>
2022-11-14 14:52:44 +0500

    pz 8 task63

---
+++ task63/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все 
+натуральные числа в промежутке от 1 до N. 
+N = 5 -> "1, 2, 3, 4, 5" 
+N = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+
+
+string Number(int m, int n)
+{
+    if (m<=n)
+    {
+        return $"{m} " + Number(m+1, n);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+Console.WriteLine(Number(m,n));
